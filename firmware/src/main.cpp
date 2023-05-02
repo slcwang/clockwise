f11572f098d2cb2f99bd8b69f9e6a1499f1992d8
slcwang <131223926+slcwang@users.noreply.github.com>
2023-05-02 17:17:28 +0800

    Update main.cpp
    Signed-off-by: slcwang <131223926+slcwang@users.noreply.github.com>

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -2,14 +2,15 @@
 #include <ESP32-HUB75-MatrixPanel-I2S-DMA.h>
 
 // Clockface
+
 #include <Clockface.h>
 // Commons
 #include <WiFiController.h>
 #include <CWDateTime.h>
-#include <CWPreferences.h>
+
 #include <CWWebServer.h>
 #include <StatusController.h>
-
+#include <CWPreferences.h>
 MatrixPanel_I2S_DMA *dma_display = nullptr;
 
 Clockface *clockface;
@@ -58,9 +59,11 @@ void setup()
   StatusController::getInstance()->wifiConnecting();
   if (wifi.begin())
   {
+    StatusController::getInstance()->wifiConnected();
+    delay(1000);
     StatusController::getInstance()->ntpConnecting();
     cwDateTime.begin(ClockwiseParams::getInstance()->timeZone.c_str(), ClockwiseParams::getInstance()->use24hFormat);
-    clockface->setup(&cwDateTime);
+    clockface->setup(&cwDateTime, ClockwiseParams::getInstance()->symbol1, ClockwiseParams::getInstance()->symbol2, ClockwiseParams::getInstance()->symbol3, ClockwiseParams::getInstance()->symbol4, ClockwiseParams::getInstance()->symbol5);
   }
 }
 
